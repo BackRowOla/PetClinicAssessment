063da76f71394bb4df4d5a70f4ad9ddaba9471ad
renJoseph <ahrennatjoseph@googlemail.com>
2019-10-25 10:59:51 +0100

    pls work

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,11 @@
-
+function toOwnersPage() {
+    window.location.href = "insert relative path";
+}
+ 
+function toPetsPage() {
+    window.location.href = "insert relative path";
+}
+ 
+function toVisitsPage() {
+    window.location.href = "insert relative path";
+}
