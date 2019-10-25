c89bd523b2f3e545d53d2bf49a87bcd2981c79ef
BackRowOla <Babalakin.Oyeyemi@academytrainee.com>
2019-10-25 14:10:27 +0100

    get user info plus pets

--- Scripts/ownerInfo.js
+++ Scripts/ownerInfo.js
@@ -1,1 +1,42 @@
+//fetch owner info by id
+let params = new URLSearchParams(window.location.search);
+let ownerId = params.get('id'); // dont forget to change to this from hard coded value
 
+getOwener();
+
+function getOwener() {
+    fetch('http://localhost:9966/petclinic/api/owners/' + 1)
+        .then(res => res.json())
+        .then(json => handleData(json))
+        .catch(err => console.error(err));
+}
+
+function handleData(data) {
+    console.log(data);
+    let fullname = data.firstName + " " + data.lastName;
+
+    setData('ownerName', fullname);
+    setData('addressLine', "Address: " + data.address);
+    setData('cityLine', "City: " + data.city);
+    setData('telephoneLine', "Phone Number: " + data.telephone);
+    getPets(data.pets);
+}
+
+function getPets(petsData) {
+
+    petsData.forEach(element => {
+        constructElement(element.id, 'li', "Name: " + element.name + " | Type: " + element.type.name);
+    });
+}
+
+function setData(elId, textData) {
+    document.getElementById(elId).innerText = textData;
+}
+
+function constructElement(petId, elementName, content) {
+    let myElement = document.createElement(elementName);
+    myElement.addEventListener('click', () => window.location.href = './petInfo.html?' + petId);
+    myElement.className = 'list-group-item';
+    myElement.innerText = content;
+    document.getElementById('petList').appendChild(myElement);
+}
