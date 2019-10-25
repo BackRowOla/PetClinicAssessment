633678babcc287adcb3dd3d32d7d83bf3a22cbce
hazzydude <haydenlamb97@gmail.com>
2019-10-25 11:19:01 +0100

    update of owners.js

--- Scripts/owners.js
+++ Scripts/owners.js
@@ -1,1 +1,10 @@
 
+function printStuff() {
+    debugger;
+    fetch('http://localhost:9966/petclinic/api/owners', {
+        method: 'GET',
+        headers: { "Accept": "application/json;charset=UTF-8" }
+
+    }).then(res => res.json())
+    .then(json => console.log(json));
+}
